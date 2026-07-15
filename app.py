b9f98cbf03093e86651dc467e4a505b9b6d993a8
Roja-Mandrakuriti <mandrakuritirojamrecw@gmail.com>
2026-07-15 11:59:28 +0530

    app.py

--- app.py
+++ app.py
@@ -75,4 +75,9 @@ with gr.Blocks(title="Image-to-Story Generator") as demo:
         outputs=output
 )
 
-demo.launch()
+import os
+
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
